739b006093f31cff0d7f30383dfdd8be3eb1f522
Ubuntu <ubuntu@ip-172-31-42-208.ec2.internal>
2019-10-20 09:25:06 +0000

    Filters view started, filter_price in html, js & css  ok

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import search_by_name
+from .views import search_by_name, filters
 
 urlpatterns = [
-    url(r'^$', search_by_name, name='search_by_name')
+    url(r'^$', search_by_name, name='search_by_name'),
+    url(r'^filters/$', filters, name='filters')
 ]
--- search/views.py
+++ search/views.py
@@ -5,3 +5,7 @@ from artifacts.models import Artifact
 def search_by_name(request):
     artifacts = Artifact.objects.filter(name__icontains=request.GET['q']) # for some reaseon dont want to get by artifact_name
     return render(request, "artifacts.html", {"artifacts": artifacts})
+
+def filters(request):
+    artifacts = Artifact.objects.filter(name__icontains=request.GET['q'])
+    return render(request, "artifacts.html", {"artifacts": artifacts})
