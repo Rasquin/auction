d8e0431322ce99ad13ac1e8b0b9d16832d9f780c
Ubuntu <ubuntu@ip-172-31-42-208.ec2.internal>
2019-10-28 09:41:05 +0000

    Heroku host added

--- auction/settings.py
+++ auction/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('AWS_C9_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('AWS_C9_HOSTNAME'), os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
