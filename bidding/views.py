497be51acba75ff6579b36b2e79c66f6ab7f1d6f
Ubuntu <ubuntu@ip-172-31-41-39.ec2.internal>
2019-12-02 12:46:00 +0000

    Add minimun bidding price functionality

--- bidding/views.py
+++ bidding/views.py
@@ -14,15 +14,19 @@ def place_bidding(request, id):
     the_artifact = get_object_or_404(Artifact, pk=id)
     new_bidding =  Decimal(request.POST.get('new_bidding'))
     
-    
+   # if the_artifact.current_bidding_price < the_artifact.minimun_bidding_price:
+    #    messages.error(request, "Your bid is not high enough")
+    #else:
     if the_artifact.current_bidding_price >= new_bidding:
         messages.error(request, "Your bid is not high enough")
-    
+            
     else:
-        the_artifact.current_bidding_price = new_bidding
-        the_artifact.by_user = request.user.id
-        
-        the_artifact.save()
-        messages.error(request, "Your bid has been successfully placed !")
+        if new_bidding < the_artifact.minimun_bidding_price:
+            messages.error(request, "Your bid is not higher than the minimun amount to bid")
+        else:
+            the_artifact.current_bidding_price = new_bidding
+            the_artifact.by_user = request.user.id
+            the_artifact.save()
+            messages.error(request, "Your bid has been successfully placed !")
 
     return redirect('/artifacts/' + id)
