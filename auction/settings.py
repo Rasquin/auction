ac9c23f3291460a4ffdfe4f5386716e5aedae619
Ubuntu <ubuntu@ip-172-31-41-39.ec2.internal>
2019-12-30 09:16:38 +0000

    changed media_root and media_url in settings.py so the images are found by the browser

--- auction/settings.py
+++ auction/settings.py
@@ -166,9 +166,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
